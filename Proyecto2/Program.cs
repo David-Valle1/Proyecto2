541fb3d74b4bf95c4c06355efd071b0f8efe0fb1
Samuel Valle <Samuelvalle010@gmail.com>
2024-09-03 21:49:53 -0600

    Agregar archivos de proyecto.

---
+++ Proyecto2/Program.cs
@@ -0,0 +1,55 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        bool Continuar = true;
+
+        while (Continuar)
+        {
+            Console.Clear();
+            Console.WriteLine("===Ventas varias===");
+            Console.WriteLine("1 Transistores");
+            Console.WriteLine("2 Capasitores");
+            Console.WriteLine("3 Leds");
+            Console.WriteLine("4 Salir");
+            Console.WriteLine("===============");
+            Console.Write("Seleccione una opción: ");
+            string option = Console.ReadLine(); 
+            
+            switch (option)
+            {
+                case "1":
+                    Console.WriteLine("Has seleccionado la opcion uno");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Has seleccionado la opcion dos");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Has seleccionado la opcion tres");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Saliendo del programa.....");
+                    Continuar = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opción no valida intente de nuevo");
+                    break;
+
+
+                if (Continuar)
+                {
+                    Console.WriteLine("presione cualquier tecla para continuar");
+                    Console.ReadKey();
+                }
+
+            }
+
+        }
+
+    }
+    
+}
